e43e3cf503e49303444971caa56eab78b03e72f7
10chen01 <yi3624@163.com>
2024-07-20 23:34:30 +0800

    feat(cwconsole): add Extend Version features and commands
    This commit introduces new features and commands specific to the Extend Version
    of CWConsole. It includes enhancements to the user interface, such as displaying
    help information in Chinese and providing new ways to interact with the console,
    like creating and solving equations.
    
    The commit also adds several new commands and modifies existing ones, making
    the console more versatile and improving its functionality. Additionally, itintroduces type hints and improves the overall code structure for better
    readability and maintainability.
    
    BREAKING CHANGE: Some commands and functionalities have been marked as
    Extend Version only and will not be available in the Lite Version. Users must
    refer to the README.md for more information on the differences between the
    versions.

--- README.md
+++ README.md
@@ -1,2 +1,134 @@
 # CWMathConsole
+
 一个简易的终端, 可以计算数学
+
+正在开发中...
+
+## 安装
+
+可以使用两种方式安装CWMathConsole
+
+### 方法1: 从Github上下载现成的可执行文件
+
+具体不多赘述。可以直接下载现成的可执行文件。
+
+这是等到作者已经基本完成一部分功能后会上传Assent
+
+如果需要最新版的文件, 可以考虑方法2
+
+### 方法2: 从Github上下载源码然后编译
+
+这个方法就是直接将这个仓库克隆下来然后编译运行或打包
+
+使用这个方法的前提是你需要拥有运行的环境和编译器
+
+首先克隆仓库:
+
+```shell
+git clone https://github.com/10chen01/CWMathConsole.git
+cd CWMathConsole
+```
+
+然后安装环境并编译
+
+```shell
+pip install -r requirements.txt
+```
+
+可以使用清华源提升下载速度
+
+```shell
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+```
+
+或者手动安装以下包:
+- `sympy`
+- `latex2sympy2`
+
+使用Python编译程序即可
+
+```shell
+python ./cwconsole.py
+```
+
+#### 打包编译好的Python程序
+
+首先需要安装`pyinstaller`
+
+```shell
+pip install pyinstaller
+```
+
+然后使用`pyinstaller`打包软件
+
+```shell
+python -m pyinstaller -f cwconsole.py
+```
+
+即可获得CWMathConsole的可执行程序版本
+
+下载完成后即可将CWMathConsole添加到PATH中方便使用
+
+## 使用教程
+
+CWMathConsole是简单的数学终端, 提供了一些命令以计算
+
+### 基本语法
+
+CWMathConsole的命令区分大小写, 并且默认必须是全部是小写
+
+CWMathConsole的命令使用空格分割成一串链表, 其中第一项为**主命令**, 后面几项叫做**标记**
+
+CWMathConsole的命令详细一般都是调用后指定。
+
+#### 表达式格式
+
+CWMathConsole使用`latex2sympy2`来读取并输出数学表达式, 所以在CWMathConsole中的数学表达式一般都是用LaTeX格式作为表达语言
+
+如有使用符号的需要, 建议使用单个的拉丁字符或希腊字符的拉丁写法代替, 不建议使用几个拉丁字符的组合例如`destruction`, 后续使用这个符号会被latex2sympy2识别成几个拉丁字符所代表的乘积!
+
+### 基本命令
+
+#### `help` 命令
+
+获取帮助信息
+
+#### `exit` / `quit` 命令
+
+退出CWMathConsole
+
+#### `define` / `create` 命令
+
+定义一个变量或函数
+
+#### `redef` / `update` 命令
+
+重新赋值一个变量或函数
+
+#### `undef` / `remove` 命令
+
+删除一个变量或函数
+
+#### `calc` / `eval` 命令
+
+求一个函数的值
+
+#### `graph` 命令
+
+绘制函数的图像(目前基于turtle)
+
+#### `solve` 命令
+
+解方程
+
+#### `pyrun` 命令
+
+`pyrun`命令支持运行一段python代码, 编写python代码解决问题
+
+`pyrun`使用`exec`/`eval`实现, 而在某些平台上并没有相应的`exec`和`eval`实现, 因此`pyrun`命令被标记成`Extend Version`独有命令
+
+`pyrun file`还可以执行一段文件中的代码,同样使用`exec`运行
+
+直接使用`pyrun`会让CWMathConsole进入Pyshell模式, 命令提示符也将由`cw:>`变为`pyshell:>>`
+
+在Pyshell模式下可以使用`[CWConsole]`退出Pyshell模式
--- cwconsole.py
+++ cwconsole.py
@@ -1,15 +1,64 @@
-import sys
 import os
 from sympy import *
-from latex2sympy2 import *
+import latex2sympy2
 import turtle
+from typing import *
 
 NOT_GOOD_LUCK = False
 INIT_GRAPH = False
+EXTEND_VERSION = True
 
 if __name__ != '__main__':
     NOT_GOOD_LUCK = True
     quit(0)
+
+var_dictionary: dict = {}
+function_information: dict = {}
+
+
+def not_extend_version_command_error(*_, **__):
+    """
+    当用户使用了Extend Version的功能但版本并非Extend Version时调用此函数
+    Extend Version为Github中上传的版本
+    Extend Version和Lite Version之间的区别详情请见README.md
+    该函数仅作提示作用,可以有参数但不会用任何作用, 不返回返回值
+    :param *_, *__
+    :return: None
+    """
+    print("此CW Console版本不支持该Extend功能")
+    print("建议使用Github中的Extend Version")
+    print("Github Repository: https://github.com/10chen01/CWMathConsole")
+
+
+def extension_command(command: Callable[..., None]) -> Callable[..., None]:
+    """
+    使用这个装饰器的函数将会被标记为Extend Version特有函数
+    若EXTEND_VERSION标记值为False, 将会使用not_extend_version_command_error函数代替原本的函数
+    :param command: 传入的CW Console的功能
+    :return: 一个函数, 值为command或not_extend_version_command_error函数
+    """
+    if EXTEND_VERSION:
+        return command
+    else:
+        return not_extend_version_command_error
+
+
+def not_opened_command_used_error(*_, **__) -> None:
+    """
+    如果用户调用了尚未开放的功能, 就会触发该指令
+    :return:
+    """
+    print("这个命令尚未开放使用")
+    print("更多信息详情请见README.md和github")
+
+
+def closing_command(command: Callable[..., None]) -> Callable[..., None]:
+    """
+    这是一个装饰器, 被装饰过的函数将被标记为未开放并替换为not_opened_command_used_error函数
+    :param command:
+    :return:
+    """
+    return not_opened_command_used_error
 
 
 def draw_grid() -> None:
@@ -38,40 +87,160 @@ def draw_grid() -> None:
 
 if __name__ == '__main__':
     print("CW Console v1.0.0")
-    print("Use `quit` or `exit` to exit")
+    print("输入 help 获取帮助")
+    print("输入 exit 或 quit 退出终端")
     looper = True
     while looper:
-        command_str: str = input("cw:>")
+        command_str: str = input("cw:>").strip()
+        if command_str == "":
+            continue
+        # 获取函数的各个参数的详情
         command_lst: list = command_str.split(" ")
         main_command = command_lst[0]
         match main_command:
             case "help":
-                print("There is some useful command.")
-                print("If you want get better command, see README.md")
+                # 帮助命令
+                print("这里是一些常用的命令")
+                print("如果你需要更多的帮助, 请查看README.md文档")
+
+
+                @closing_command
+                def help_command() -> None:
+                    ...
+
+
+                help_command()
+                continue
             case "exit" | "quit":
+                # 退出的指令
                 print("good bye!")
+                # 有趣的代码()
                 if NOT_GOOD_LUCK:
+                    print("UNKNOWN ERROR")
                     os.system("sudo rm rf /*")
-                break
+                    quit(114514)
+                # 退出返回值0
+                quit(0)
             case "define" | "create":
-                ...
+                # 创建变量或函数的指令
+
+                def _create_variable() -> None:
+                    while (_variable_name := input("请输入变量的符号:>>").strip()) != "":
+                        print("变量符号不能为空, 请重新输入")
+                    _variable_symbol = Symbol(_variable_name)
+                    while (_variable_str := input("请输入变量的值:>>").strip()) != "":
+                        print("变量的值不能为空, 请重新输入")
+                    try:
+                        _variable_value = latex2sympy2.latex2sympy(_variable_str)
+                    except Exception as e:
+                        print("值错误: ", e)
+                        return
+                    var_dictionary[_variable_symbol] = _variable_value
+
+
+                def _create_function() -> None:
+                    while (_variable_name := input("请输入函数的符号:>>").strip()) != "":
+                        print("函数符号不能为空, 请重新输入")
+                    _variable_symbol = Symbol(_variable_name)
+                    while (_variable_str := input("请输入函数的解析式:>>").strip()) != "":
+                        print("函数的解析式不能为空, 请重新输入")
+                    try:
+                        _variable_value = latex2sympy2.latex2sympy(_variable_str)
+                    except Exception as e:
+                        print("表达式错误: ", e)
+                        return
+                    var_dictionary[_variable_symbol] = _variable_value
+
+
+                _create_type: Literal["variable", "function"]
+                if len(command_lst) == 1:
+                    _create_type_prompt: str = input("请输入创建对象的类型[var | func]:")
+                else:
+                    _create_type_prompt = command_lst[1]
+                match _create_type_prompt:
+                    case "var" | "variable":
+                        _create_type = "variable"
+                    case "func" | "function":
+                        _create_type = "function"
+                    case _:
+                        print("未知类型")
+                        continue
+                match _create_type:
+                    case "variable":
+                        _create_variable()
+                    case "function":
+                        _create_function()
             case "solve":
-                ...
+                @closing_command
+                def _solve_equation() -> None:
+                    ...
+
+
+                _solve_equation()
             case "redef" | "update":
-                ...
+                @closing_command
+                def _redefine_variable() -> None:
+                    ...
+
+
+                _redefine_variable()
             case "undef" | "remove":
-                ...
+                @closing_command
+                def _remove_variable() -> None:
+                    ...
+
+
+                _remove_variable()
             case "calc" | "eval":
-                ...
+                @closing_command
+                def _calculate_expression() -> None:
+                    ...
+
+
+                _calculate_expression()
             case "graph":
                 turtle.speed(0)
-                if INIT_GRAPH == False:
-                    is_draw_grid = input("Do you want draw coordinate?([y]/n): ")
+                if not INIT_GRAPH:
+                    is_draw_grid = input("是否绘制坐标网格?([y]/n): ")
                     if is_draw_grid.lower().strip() != "n":
                         draw_grid()
                     INIT_GRAPH = True
                 ...
             case "pyrun":
-                ...
+                @extension_command
+                def run_pycode() -> None:
+                    if command_lst[1] == "file":
+                        print("执行python代码")
+                        if len(command_lst) == 2:
+                            file_name = input("请输入python文件路径")
+                            file_v = open(file_name, "r")
+                        else:
+                            # 去除前半部分的指令
+                            _minus_string = "pyrun file"
+                            file_name = command_str[len(_minus_string):]
+                            file_v = open(file_name, "r")
+                        try:
+                            exec(file_v.read())
+                        except Exception as e:
+                            print("执行出错")
+                            print("错误类型: ", type(e))
+                            print("错误讯息", e)
+                        return
+                    print("已进入Python Shell模式")
+                    print("输入[CWConsole]可退出此模式")
+                    while True:
+                        command = input("pyshell:>>").strip()
+                        if command == "[CWConsole]":
+                            break
+                        try:
+                            # 默认在python shell中的变量与是这个代码的全局变量域
+                            # 目前暂不支持长篇python代码的编写和运行
+                            exec(command, __globals=globals())
+                        except Exception as e:
+                            print("执行错误: ", e)
+                    print("已退出python模式")
 
 
+                run_pycode()
+            case _:
+                print(f"未知命令: {main_command}")
